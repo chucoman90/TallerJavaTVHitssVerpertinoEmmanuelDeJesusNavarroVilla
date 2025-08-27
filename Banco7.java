9ef15b170795229e61fe3c8e175b666a15188463
chucoman90 <43760541+chucoman90@users.noreply.github.com>
2025-08-27 08:02:57 -0600

    Update Banco7.java

--- Banco7.java
+++ Banco7.java
@@ -9,10 +9,14 @@ public class Banco7 {
 
         // Registrar transacciones
         Transaccion7 t1 = depositar(ana, 2000);
-        Transaccion7 t2 = retirar(ana, 1000);
+        Transaccion7 t2 = retirar(ana, 8000);
+        if(t2 == null)
+            System.out.println("No se pudo realizar el retiro por fondos insuficientes.");
         Transaccion7 t3 = retirar(luis, 3000);
+        if (t3 == null)
+            System.out.println("No se pudo realizar el retiro por fondos insuficientes.");
         Transaccion7 t4 = depositar(luis, 1500);
-
+        
         // Mostrar información de cuentas
         System.out.println("=== Detalle de Cuentas ===");
         mostrarCuenta(ana);
@@ -35,9 +39,10 @@ public class Banco7 {
         return new Transaccion7(Transaccion7.TipoTransaccion.DEPOSITO, monto, cuenta);
     }
 
-    public static Transaccion7 retirar(CuentaBancaria7 cuenta, double monto) {
-        cuenta.retirar(monto);
-        return new Transaccion7(Transaccion7.TipoTransaccion.RETIRO, monto, cuenta);
+     public static Transaccion7 retirar(CuentaBancaria7 cuenta, double monto) {
+        if(cuenta.retirar(monto) == true)
+            return new Transaccion7(Transaccion7.TipoTransaccion.RETIRO, monto, cuenta);
+        return null;
     }
 
     public static void mostrarCuenta(CuentaBancaria7 cuenta) {
@@ -48,3 +53,4 @@ public class Banco7 {
         System.out.println(transaccion);
     }
 }
+
